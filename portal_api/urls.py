d124142345274e126130396e16d4a81b6861bc01
MrLobotomist <smehovpavel@gmail.com>
2024-07-11 23:58:40 +0500

    cors

--- portal_api/urls.py
+++ portal_api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from portal_api.api_views.auth import KerberosAuthView
 from portal_api.views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -15,6 +17,7 @@ router.register(r'groups', GroupViewSet)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/kerberos/', KerberosAuthView.as_view(), name='kerberos_auth'),
     path('', include(router.urls)),
 ]
 
